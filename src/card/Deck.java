84112a6694e847ebb86f93d34dd2625e263103fd
Daniel Winkler <daniel.winkler3@gmail.com>
2020-04-02 18:02:51 -0700

    Player stuff

--- src/card/Deck.java
+++ src/card/Deck.java
@@ -1,45 +1,22 @@
 package card;
-import player.Player;
 import java.util.ArrayList;
 import java.util.Collections;
 import global.GlobalShit;
 
 public class Deck {
-    ArrayList<Card> cards;
-    ArrayList<Player> players;
-
-    public Deck(ArrayList<Player> players) {
-        this.players = players;
-        cards = new ArrayList<>();
-        for (int shape = 0; shape < 4; shape++) {
-            for (int num = 2; num < 15; num++) {
+    ArrayList cards;
+    public Deck(){
+        cards= new ArrayList<Card>();
+        for (int shape = 0; shape < 4 ; shape++) {
+            for (int num = 2; num < 15 ; num++) {
                 cards.add(new Card(shape, num));
             }
         }
     }
-
-    public void shuffle() {
+    public void shuffle(){
         Collections.shuffle(cards);
     }
+    public void deal(){
 
-    public void deal() {
-        shuffle();
-        for (int i = 0; i < players.size(); i++) {
-            Player p = players.get(i);
-            Card[] hand = new Card[13];
-            for (int j = 13 * i; j < 13 * (i + 1); j++) {
-                hand[j - (13 * i)] = cards.get(j);
-            }
-            p.setHand(hand);
-        }
-
-    }
-
-    public ArrayList getCards() {
-        return cards;
-    }
-
-    public void setCards(ArrayList<Card> cards) {
-        this.cards = cards;
     }
 }
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,7 +1,6 @@
 package player;
 
 import card.Card;
-
 import java.util.Scanner;
 
 public class Player {
@@ -9,16 +8,15 @@ public class Player {
     private int score;
     private Hand hand;
 
+    Scanner in = new Scanner(System.in);
+
     public void setHand(Card[] card){
         hand.setHand(card);
     }
 
     Card getCard(){
         System.out.println("Pick a card from your hand");
-
         int shape, num;
-        Scanner in = new Scanner(System.in);
-
         System.out.println("Enter number 2-14");
         num = in.nextInt();
         while (!(num >=2 && num <=14)){
@@ -29,13 +27,33 @@ public class Player {
         System.out.println("Enter the shape, 0-4");
         System.out.println("0 - Diamond, 1 - Clubs, 2 - Hearts, 3 - Spades");
         shape = in.nextInt();
-        while (!(shape >=0 && shape <=3)){
-            System.out.println("Not a valid number, Enter number 2-14");
-            num = in.nextInt();
-        }
+        validateShape(shape);
 
         Card newCard = new Card(shape,num);
         // if() card is valid, calling a method in Hand.
         return newCard;
+    }
+
+    int getPrediction(){
+        int prediction;
+        System.out.println("Pick a prediction based on Trump (2 - 14)");
+        prediction = in.nextInt();
+        validateNumber(prediction);
+        return prediction;
+    }
+
+    void validateNumber(int num){
+        while (!(num >=2 && num <=14)){
+            System.out.println("Not a valid number, Enter number 2-14");
+            num = in.nextInt();
+        }
+    }
+
+
+    void validateShape(int num){
+        while (!(num >=0 && num <=3)){
+            System.out.println("Not a valid number, Enter number 0-3");
+            num = in.nextInt();
+        }
     }
 }
