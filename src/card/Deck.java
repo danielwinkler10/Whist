7a4072f57d4a95caa62874b9e099a3f2bde03cbd
rdagan1 <roydagan2002@gmail.com>
2020-04-02 19:58:09 -0700

    DEAL REINSTATED no error

--- src/card/Deck.java
+++ src/card/Deck.java
@@ -1,33 +1,38 @@
 package card;
+
+import player.Player;
+
 import java.util.ArrayList;
 import java.util.Collections;
-import global.GlobalShit;
-import player.Player;
 
 public class Deck {
     ArrayList<Card> cards;
     ArrayList<Player> players;
-    public Deck(ArrayList<Player> players){
-        cards= new ArrayList<>();
-        this.players= players;
-        for (int shape = 0; shape < 4 ; shape++) {
-            for (int num = 2; num < 15 ; num++) {
+
+    public Deck(ArrayList<Player> players) {
+        cards = new ArrayList<>();
+        this.players = players;
+        for (int shape = 0; shape < 4; shape++) {
+            for (int num = 2; num < 15; num++) {
                 cards.add(new Card(shape, num));
             }
         }
     }
-    public void shuffle(){
+
+    public void shuffle() {
         Collections.shuffle(cards);
     }
-    public void deal(){
-        shuffle();
-        for (int i=0; i<players.size(); i++) {
-            Player p= players.get(i);
-            Card[] hand= new Card[13];
-            for (int j = 13*i; j <13*(i+1) ; j++) {
-                hand[j-(13*i)]=cards.get(j);
 
-            p.setHand(hand);
+    public void deal() {
+        shuffle();
+        for (int i = 0; i < players.size(); i++) {
+            Player p = players.get(i);
+            Card[] hand = new Card[13];
+            for (int j = 13 * i; j < 13 * (i + 1); j++) {
+                hand[j - (13 * i)] = cards.get(j);
+
+                p.setHand(hand);
+            }
         }
     }
 }
