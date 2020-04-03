fc7e9149be60916e511a749d7316a9455e4cbf01
Shahar <shahar.broner@gmail.com>
2020-04-02 18:56:34 -0700

    Continue Round class

--- src/game/Round.java
+++ src/game/Round.java
@@ -8,12 +8,12 @@ import java.util.ArrayList;
 
 public class Round {
     private int miniDonald;
-    ArrayList<Card> onTable= new ArrayList();
-    ArrayList<Player> players= Main.getPlayers();
-    Player starter;
+    ArrayList<Card> onTable = new ArrayList();
+    ArrayList<Player> players = Main.getPlayers();
+    int starter;
 
-    public Round(Player starter){
-        this.starter=starter;
+    public Round(int starter) {
+        this.starter = starter;
     }
 
     public int getMiniDonald() {
@@ -31,10 +31,33 @@ public class Round {
     public void setOnTable(ArrayList<Card> onTable) {
         this.onTable = onTable;
     }
-    public int playRound(){
-        for (int i = 0; i < players.size(); i++) {
-players.get(i).getCard();
+
+    public int playRound() {
+        Card firstCard = players.get(starter).getCard();
+        onTable.add(players.get(starter).getCard());
+        miniDonald = firstCard.getShape();
+        for (int i = starter + 1; i < (players.size() + starter); i++) {
+            Card c = players.get(i).getCard();
+            if (c.getShape() = Game.donald) {
+                c.setNum(c.getNum() + 13);
+            } else if (c.getShape() != miniDonald) {
+                c.setNum(0);
+            }
+            onTable.add(players.get(i).getCard());
         }
+       return (maxIndex(onTable)+starter);
+    }
+
+    public int maxIndex(ArrayList<Card> cards) {
+        int max = -1;
+        int maxIndex = -1;
+        for (int i = 0; i < cards.size(); i++) {
+            if (cards.get(i).getNum()>max){
+                max=cards.get(i).getNum();
+                maxIndex=i;
+            }
+        }
+        return maxIndex;
     }
 
 }
