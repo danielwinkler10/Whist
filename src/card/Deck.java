0a9305244d3d16443e1eeedcc32d0ed1ec36fd4c
rdagan1 <roydagan2002@gmail.com>
2020-04-02 17:45:37 -0700

    deal method done

--- src/card/Deck.java
+++ src/card/Deck.java
@@ -1,10 +1,14 @@
 package card;
+import player.Hand;
+import player.Player;
 import java.util.ArrayList;
 import java.util.Collections;
 
 public class Deck {
     ArrayList<Card> cards;
-    public Deck(){
+    ArrayList<Player> players;
+    public Deck(ArrayList<Player> players){
+        this.players=players;
         cards= new ArrayList<>();
         for (int shape = 0; shape <4 ; shape++) {
             for (int num = 2; num <15 ; num++) {
@@ -16,6 +20,15 @@ public class Deck {
         Collections.shuffle(cards);
     }
     public void deal(){
+        shuffle();
+        for (int i=0; i<players.size(); i++) {
+            Player p= players.get(i);
+            Card[] hand= new Card[13];
+            for (int j = 13*i; j <13*(i+1) ; j++) {
+               hand[j-(13*i)]=cards.get(j);
+            }
+            p.setHand(hand);
+        }
 
     }
 
