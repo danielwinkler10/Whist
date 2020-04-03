b8abb4094abb7ea53af3916b0a444050fe67dba0
MrGabarel <gharel620@student>
2020-04-02 23:47:44 -0700

    Fixed bug in createPlayers

--- src/driver/Main.java
+++ src/driver/Main.java
@@ -8,28 +8,25 @@ import player.Player;
 import java.util.ArrayList;
 
 public class Main {
-  static  ArrayList<Player> players = new ArrayList<>();
+    static ArrayList<Player> players = new ArrayList<>();
 
     public static void main(String[] args) {
-      createPlayers(GlobalShit.SHAPE_COUNT);
+        createPlayers(GlobalShit.SHAPE_COUNT);
 
-      Deck deck = new Deck(players);
+        Deck deck = new Deck(players);
 
-      for (int i =0; i < players.size(); i++){
-          Game game = new Game(players, i, deck);
-      }
-
-
+        for (int i = 0; i < 1; i++) {
+            Game game = new Game(players, i, deck);
+        }
     }
 
     public static ArrayList<Player> getPlayers() {
         return players;
     }
 
-    public static ArrayList<Player> createPlayers(int numPlayers){
-        ArrayList<Player> players= new ArrayList<>();
-        for (int i = 0; i <numPlayers ; i++) {
-         Player p = new Player("Player"+i);
+    public static ArrayList<Player> createPlayers(int numPlayers) {
+        for (int i = 0; i < numPlayers; i++) {
+            Player p = new Player("Player" + i);
             players.add(p);
         }
 
