27d4b9ca39f86a1803b2f5270a40628fe5389eec
Daniel Winkler <daniel.winkler3@gmail.com>
2020-04-04 21:37:44 -0700

    Hand comments

--- src/player/Hand.java
+++ src/player/Hand.java
@@ -19,9 +19,11 @@ public class Hand {
         this.cards = cards;
     }
 
-    // Method to check if a chosen card is in player's hand.
+    // Method to check if a chosen card is in player's hand
     public boolean isInHand(Card cardToCheck) {
+        //Loops through all of the player's cards
         for (int i = 0; i < cards.size(); i++) {
+            // Checks if both shape & number are found in a card in the player's hand
             if (cards.get(i).getNum() == cardToCheck.getNum() && cards.get(i).getShape() == cardToCheck.getShape()) {
                 return true;
             }
@@ -29,9 +31,11 @@ public class Hand {
         return false;
     }
 
-    // Method to check if player has the required shape.
+    // Method to check if player has the required shape
     public boolean hasShape(int shape) {
+        //Loops through all of the player's cards
         for (int i = 0; i < cards.size(); i++) {
+            // Checks if player has shape in hand
             if (cards.get(i).getShape() == shape) {
                 return true;
             }
@@ -39,17 +43,26 @@ public class Hand {
         return false;
     }
 
-    // Method to sort the player's hand.
+    // Method to sort the player's hand
     public void sortHand() {
+        // An ArrayList to put the cards in order
         ArrayList<Card> organizedCards = new ArrayList<>();
+        // Loop through the numbers
         for (int i = 0; i < GlobalShit.NUMBER_COUNT; i++) {
             int currentMinIndex = 0;
+            // Loop through the cards in hand
             for (int j = 1; j < cards.size(); j++) {
-                if (cards.get(currentMinIndex).getShape() > cards.get(j).getShape()) currentMinIndex = j;
-                else if (cards.get(currentMinIndex).getShape() == cards.get(j).getShape() && cards.get(currentMinIndex).getNum() > cards.get(j).getNum())  currentMinIndex = j;
+                // Puts the cards in order based on number and shape
+                if (cards.get(currentMinIndex).getShape() > cards.get(j).getShape()){
+                    currentMinIndex = j;
+                } else if (cards.get(currentMinIndex).getShape() == cards.get(j).getShape() && cards.get(currentMinIndex).getNum() > cards.get(j).getNum()){
+                    currentMinIndex = j;
+                }
             }
+            // Add card to the ArrayList in order
             organizedCards.add(cards.remove(currentMinIndex));
         }
+        // Old cards array = Organized ArrayList
         cards = organizedCards;
     }
 
